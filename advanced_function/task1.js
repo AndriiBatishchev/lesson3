bf584a1a0bf4f9eb9c6ed6405c7ad2827fc94da3
Andrii Batishchev <dvvp1998@gmail.com>
2025-05-01 01:02:30 +0300

    formatting done

--- advanced_function/task1.js
+++ advanced_function/task1.js
@@ -4,26 +4,26 @@
 // число
 // Колбек функцію яку треба викликати якщо передане число парне
 // Колбек функцію яку треба викликати якщо передане число непарне
-// Створіть ще дві функції які ви будете передавати у якості колбеків, наприклад handleEven та handleOdd. 
-// Кожна з них має виводити просте повідомлення в консоль. 
+// Створіть ще дві функції які ви будете передавати у якості колбеків, наприклад handleEven та handleOdd.
+// Кожна з них має виводити просте повідомлення в консоль.
 // Наприклад handleEven буде виводити текст “number is even”, a handleOdd буде виводити текст “number is odd”
 // Викличте функцію handleNum і передайте в якості аргументів довільне число і дві функції які ви створили раніше
 
 function handleNum(number, evenCall, oddCall) {
-    if (number % 2 === 0) {
-      evenCall(); 
-    } else if (number % 2 !== 0){
-      oddCall();
-    }
+  if (number % 2 === 0) {
+    evenCall();
+  } else if (number % 2 !== 0) {
+    oddCall();
   }
-
-function numberEven(){
-    console.log('number is even');
 }
 
-function numberOdd(){
-    console.log('number is odd');
+function numberEven() {
+  console.log("number is even");
 }
 
-handleNum(1, numberEven, numberOdd); 
+function numberOdd() {
+  console.log("number is odd");
+}
+
+handleNum(1, numberEven, numberOdd);
 handleNum(2, numberEven, numberOdd);
